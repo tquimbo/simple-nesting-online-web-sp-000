23f15055f342ac8c904a80adc4f179e2d1b01a4b
Tim Quimbo <tquimbo1@gmail.com>
2021-03-04 20:59:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,10 +63,10 @@ end
 
 def adding_matz
 
-programmer_hash[:yukihiro_matsumoto] => {
+programmer_hash[[:yukihiro_matsumoto] => {
   :known_for => "Ruby",
    :languages => ["LISP", "C"]
-}
+}]
 
 	programmer_hash =
  		{
